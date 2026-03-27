fa45aaf264dd6624d941fefc1a5270eee009661a
jpearleverett <jpearleverett@gmail.com>
2026-03-26 22:25:59 -0400

    Increase play button height from 110 to 120

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -691,7 +691,7 @@ const styles = StyleSheet.create({
   playButtonWrapper: {
     position: 'relative',
     width: '100%',
-    height: 110,
+    height: 120,
     marginBottom: 12,
   },
   playButtonImage: {
